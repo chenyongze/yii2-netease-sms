fe78be3fc3f7db21a76776ff45674e2fdd7cd965
chenlin <ujschenlin@gmail.com>
2016-09-20 17:54:14 +0800

    update

--- Sms.php
+++ Sms.php
@@ -1,30 +1,61 @@
 <?php
-
 namespace bikerr;
-
-/**
- * This is just an example.
- */
+use linslin\yii2\curl\Curl;
 class Sms {
 
-	private $appkey = '';
-	private $nonce = '';
-	private $curtime = '';
-	private $checksum = '';
+	private $AppKey = '852a8de556b5f646a34c760a52661b10';
+	private $AppSecret = 'df6a01417758';
 	
-	public function __construct( $appkey , $nonce , $curtime  ) {
-		$this->appkey = $appkey;
-		$this->nonce = $appkey;
-		$this->curtime = $curtime;
-		$this->checksum = $this->getChecksum();
+	private $Nonce = '1322023123456103123156';
+	private $CurTime = '';
+	private $CheckSum = '';
+	
+	public  $header = [];
+	
+	public function __construct(  ) {
+// 		$this->AppKey = $appkey;
+// 		$this->Nonce = $appkey;
+// 		$this->CurTime = $curtime;
+		$this->CheckSum = $this->setChecksum();
+		$this->CurTime = $this->setCurTime();
 		
+		$this->header = $this->setHeader();
 	}
 	
-	
-	public function getChecksum() {
-		return sha1($this->appkey . $this->nonce . $this->curtime);
+	private  function setCurTime() {
+		return time();
 	}
 	
+	private function setChecksum() {
+		return sha1($this->AppSecret . $this->Nonce . $this->CurTime);
+	}
 	
-	
+	private function setHeader() {
+		$header = [];
+		$header['AppKey'] 	=	$this->AppKey;
+		$header['Nonce']	=	$this->Nonce;
+		$header['CurTime']	=	$this->CurTime;
+		$header['CheckSum']	=	$this->CheckSum;
+		return $header;
+	}
+
+	public function sendVerificationCode( $mobile ) {
+		$url = 'https://api.netease.im/sms/sendcode.action';
+		$curl = new Curl();
+		$curl->setOption(CURLOPT_HTTPHEADER, $this->header)
+				->setOptions(['mobile' => $mobile])
+				->post($url);
+	}
+
+	public function sendMessage() {
+		$url = 'www.baidu.com';
+		$curl = new Curl();
+		$result = $curl->get($url);
+		var_dump($result);
+	}
 }
+
+$model = new Sms();
+$model->sendMessage();
+
+
